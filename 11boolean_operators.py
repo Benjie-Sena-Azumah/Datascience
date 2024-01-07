efabbe5bf3cf75059d06994cc9da03eae1121ec8
Jason Joy Atsu Winmorre <jasonjoywinmorre@gmail.com>
2024-01-07 16:08:02 +0000

    Programming for engineering students

---
+++ 11boolean_operators.py
@@ -0,0 +1,22 @@
+# Python logical operators [ and or not]
+# Boolean operators are used to combine conditional statements
+
+
+# and
+
+# or
+
+# not
+
+
+Valid_age = 18
+Valid_country ='Ghana'
+
+Age = int(input("Enter your age: "))
+country = input("Enter your country: ")
+
+if Age >= Valid_age and country == Valid_country:
+    print("You are eligible to vote in  Ghana")
+else:
+    print("You are not eligible to vote in Ghana")
+
---
+++ 16loop.py
@@ -0,0 +1,27 @@
+# Loop
+# For loop
+
+x =[10, 2,3,4]
+sum=0
+for i in x:
+    sum += i
+print(f'The sum is {sum}')
+
+    
+
+
+# While loop
+
+# Loop through List
+
+# Loop through multiple Lists simultaneously using zip nethod
+
+
+# range method and loops
+
+
+# Loop through Dictionary
+
+# Exercise
+# 1. Find all the even numbers in a range
+# 2. Find the sum of all elements in a list
